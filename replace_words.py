de33abd7c30c67d98d381038766a35980000015d
Dhaya <dhayagithub@gmail.com>
2024-09-19 22:05:59 +0530

    Modified the code to use just string instead of list for mapping

--- replace_words.py
+++ replace_words.py
@@ -7,16 +7,15 @@ def replaceWords(dictionary, sentence):
     for i in dictionary:
         for j in split_sentence:
             if j.startswith(i) and j not in maps:
-                maps[j] = [i]
+                maps[j] = i
             elif j.startswith(i) and j in maps:
-                maps[j].append(i)
+                maps[j] = i if len(maps[j]) > len(i) else maps[j]
 
     # to append all the words in order they appear in sentence
     for i in split_sentence:
         # replace the word if there's an alternative
         if i in maps:
-            maps[i].sort()
-            result += " " + maps[i][0]  # sorted list for getting the shortest root word
+            result += " " + maps[i]  # sorted list for getting the shortest root word
         else:
             result += " " + i
 
